6a1312c50b37ae6d71136ec35090ee01e7981470
josiahngugi <mainajosiahdev@gmail.com>
2023-07-05 08:33:59 -0700

    props

---
+++ test1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Greetings from './components/greetings';
+
+function App() {
+  
+
+  return (
+    <>
+      <Greetings name="maina" />
+    </>
+  )
+}
+
+export default App
---
+++ test1/src/components/greetings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Greetings(props){
+
+    return(
+        <>
+        <h1>hello {props.name}</h1>
+        </>
+    )
+}
+
+export default Greetings
